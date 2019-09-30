f032a1071c1d4259e1e8e3996c4ee02fda09ba38
AnastasiaSK <nastassya.karachun@gmail.com>
2019-10-01 00:44:13 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+    const bracket = {};
+
+    bracketsConfig.forEach(element => {
+        bracket[element[1]] = element[0];
+    });
+
+    const stack = [];
+    stack.push(str[0]);
+    for (let index = 1; index < str.length; index++) {
+        let openBracket = bracket[str[index]];
+
+        if (
+            openBracket !== undefined &&
+            stack[stack.length - 1] === openBracket
+        ) {
+            stack.length = stack.length - 1;
+        } else {
+            stack.push(str[index]);
+        }
+    }
+    return stack.length === 0;
 }
+module.exports = check;
